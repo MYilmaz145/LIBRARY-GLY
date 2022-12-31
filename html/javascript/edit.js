5c448e04714b5553f77b2fbe35d6f369f6211853
Jameslilja <89815334+Jameslilja@users.noreply.github.com>
2022-12-31 14:36:43 +0100

    nada

--- html/javascript/edit.js
+++ html/javascript/edit.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-    $("#edit-btn").click(function(){
-        $("#myModal").modal();
-    });
-});
+document.getElementById("edit-btn").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+
+document.getElementById("edit-btn1").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+
+document.getElementById("edit-btn2").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+
+function submitText(){
+    let titleInput = document.getElementById('titleInput').value;
+    let message = document.getElementById('commentInput').value;
+    console.log(titleInput);
+    console.log(message);
+    document.getElementById("submit").style.display = "none";
+    document.querySelector(".modal-box").style.display = "none";
+}
--- html/javascript/images.js
+++ html/javascript/images.js
@@ -6,14 +6,14 @@ var arr = new collection(['city.jpg','logo.png','naturbild.gif']);
 function openFullImgg(pic){
     fullImgBox.style.display = "flex";
     fullImgg.src = pic;
-    document.getElementById("1").style.display="none"
-    document.getElementById("2").style.display="none"
-    document.getElementById("3").style.display="none"
+    document.getElementById("edit-btn").style.display="none"
+    document.getElementById("edit-btn1").style.display="none"
+    document.getElementById("edit-btn2").style.display="none"
 }
 
 function closeFullImgg() {
     fullImgBox.style.display = "none";
-    document.getElementById("1").style.display="inline"
-    document.getElementById("2").style.display="inline"
-    document.getElementById("3").style.display="inline"
+    document.getElementById("edit-btn").style.display="inline"
+    document.getElementById("edit-btn1").style.display="inline"
+    document.getElementById("edit-btn2").style.display="inline"
 }
