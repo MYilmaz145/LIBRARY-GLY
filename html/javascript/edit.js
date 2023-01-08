c037f1662baa1503cc33cefe60dc44ebd205e8a9
Jameslilja <89815334+Jameslilja@users.noreply.github.com>
2023-01-08 20:45:23 +0100

    stuck on edit and save

--- html/javascript/edit.js
+++ html/javascript/edit.js
@@ -1,35 +1,37 @@
-document.getElementById("edit-btn").addEventListener("click", function(){
+document.getElementById("1").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+document.getElementById("2").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+document.getElementById("3").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+document.getElementById("4").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+document.getElementById("5").addEventListener("click", function(){
+    document.querySelector(".modal-box").style.display = "flex";
+    document.getElementById("submit").style.display = "flex";
+})
+document.getElementById("6").addEventListener("click", function(){
     document.querySelector(".modal-box").style.display = "flex";
     document.getElementById("submit").style.display = "flex";
 })
 
-document.getElementById("edit-btn1").addEventListener("click", function(){
-    document.querySelector(".modal-box").style.display = "flex";
-    document.getElementById("submit").style.display = "flex";
-})
-
-document.getElementById("edit-btn2").addEventListener("click", function(){
-    document.querySelector(".modal-box").style.display = "flex";
-    document.getElementById("submit").style.display = "flex";
-})
-
-document.getElementById("edit-btn3").addEventListener("click", function(){
-    document.querySelector(".modal-box").style.display = "flex";
-    document.getElementById("submit").style.display = "flex";
-})
-
-document.getElementById("edit-btn4").addEventListener("click", function(){
-    document.querySelector(".modal-box").style.display = "flex";
-    document.getElementById("submit").style.display = "flex";
-})
-document.getElementById("edit-btn5").addEventListener("click", function(){
-    document.querySelector(".modal-box").style.display = "flex";
-    document.getElementById("submit").style.display = "flex";
-})
 
 function submitText(){
+    const submit = document.getElementById('submit')[0];
     let titleInput = document.getElementById('titleInput').value;
     let message = document.getElementById('commentInput').value;
+    if (submit == '1'){
+        titleInput = $(".title2").innerHTML;
+        message = $(".comment2").innerHTML;
+    }
     console.log(titleInput);
     console.log(message);
     document.getElementById("submit").style.display = "none";
--- html/javascript/images.js
+++ html/javascript/images.js
@@ -6,20 +6,22 @@ var arr = new collection(['allianz.jpg','oldTraffold.jpg','olympic.jpg','Tele2.j
 function openFullImgg(pic){
     fullImgBox.style.display = "flex";
     fullImgg.src = pic;
-    document.getElementById("edit-btn").style.display="none"
-    document.getElementById("edit-btn1").style.display="none"
-    document.getElementById("edit-btn2").style.display="none"
-    document.getElementById("edit-btn3").style.display="none"
-    document.getElementById("edit-btn4").style.display="none"
-    document.getElementById("edit-btn5").style.display="none"
+    document.getElementById("album-grid").style.display="none"
+    document.getElementById("1").style.display="none"
+    document.getElementById("2").style.display="none"
+    document.getElementById("3").style.display="none"
+    document.getElementById("4").style.display="none"
+    document.getElementById("5").style.display="none"
+    document.getElementById("6").style.display="none"
 }
 
 function closeFullImgg() {
     fullImgBox.style.display = "none";
-    document.getElementById("edit-btn").style.display="inline"
-    document.getElementById("edit-btn1").style.display="inline"
-    document.getElementById("edit-btn2").style.display="inline"
-    document.getElementById("edit-btn3").style.display="inline"
-    document.getElementById("edit-btn4").style.display="inline"
-    document.getElementById("edit-btn5").style.display="inline"
+    document.getElementById("1").style.display="inline"
+    document.getElementById("2").style.display="inline"
+    document.getElementById("3").style.display="inline"
+    document.getElementById("4").style.display="inline"
+    document.getElementById("5").style.display="inline"
+    document.getElementById("6").style.display="inline"
+    document.getElementById("album-grid").style.display="grid"
 }
